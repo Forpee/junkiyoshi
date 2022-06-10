6ff1c78e2bdad80e17b95cb0ccfeb6ea32845a40
Forpee <khalilhassam3@gmail.com>
2022-06-10 19:12:59 +0200

    a

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -4,5 +4,12 @@ varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor = vec4(vUv, 1.0, 1.0);
+    float width=.03;
+    
+    float prec=.001;
+    
+    float border=max(
+        smoothstep(width+prec,width-prec,vUv.x),
+    )
+    gl_FragColor=vec4(vUv,1.,1.);
 }
